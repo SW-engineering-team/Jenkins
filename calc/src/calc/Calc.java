90b1e506664b56e3a1ce13e514eee1012bad80d9
정승윤 <jswun123@g.hongik.ac.kr>
2026-06-02 14:53:00 +0900

    Rtrert

---
+++ calc/src/calc/Calc.java
@@ -0,0 +1,23 @@
+package calc;
+
+public class Calc {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+        }
+        return (double) a / b;
+    }
+}
